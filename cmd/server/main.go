e9de880d7842aaf25b7e68eeb33ff00d5b14ecd7
Nakohartum <artemofgta@gmail.com>
2026-02-16 20:49:25 +0500

    fix

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -24,14 +24,12 @@ func main() {
 	metricsHandler := handler.NewMetricsHandler(metricsService)
 	router.Use(middleware.StripSlashes)
 
-	router.Post("/update/{metricType}/{metricName}/{metricValue}", logger.AttachLoggingToResponse(metricsHandler.SetMetricDataHandle()))
 	router.Route("/", func(r chi.Router) {
 		r.Get("/", metricsHandler.ServePage)
-		r.Post("/update/", logger.AttachLoggingToResponse(metricsHandler.UpdateMetricsDataHandle()))
+		r.Post("/update", logger.AttachLoggingToResponse(metricsHandler.UpdateMetricsDataHandle()))
 		
-		r.Route("/value/", func(r chi.Router) {
+		r.Route("/value", func(r chi.Router) {
 			r.Post("/", logger.AttachLoggingToResponse(metricsHandler.GetMetricsByNameHandle()))
-			r.Get("/{metricType}/{metricName}", logger.AttachLoggingToResponse(metricsHandler.GetMetricDataHandle()))
 		})
 	})
 
