2562d9b09242422d5df945dd6494f9ad8e603853
Nakohartum <artemofgta@gmail.com>
2026-02-16 20:37:13 +0500

    fix: add metrics answer

--- internal/handler/mertichanlder.go
+++ internal/handler/mertichanlder.go
@@ -3,10 +3,12 @@ package handler
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"html/template"
 	"net/http"
 	"strconv"
 
+	config "github.com/Nakohartum/practicum-metrics/internal/config/memstorage"
 	models "github.com/Nakohartum/practicum-metrics/internal/model"
 	"github.com/Nakohartum/practicum-metrics/internal/repository"
 	"github.com/Nakohartum/practicum-metrics/internal/service"
@@ -70,7 +72,6 @@ func (mh *MetricsHandler) GetMetricsByNameHandle() http.Handler {
 	fun := func(w http.ResponseWriter, r *http.Request) {
 		var metricToSearch models.Metrics
 		var buf bytes.Buffer
-
 		_, err := buf.ReadFrom(r.Body)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
@@ -89,17 +90,53 @@ func (mh *MetricsHandler) GetMetricsByNameHandle() http.Handler {
 			http.Error(w, "no metric found", http.StatusNotFound)
 			return
 		}
-		responseData, err := json.Marshal(metricData)
+
+		resMetrics, err := makeMetricsResult(metricToSearch.MType, metricData)
+		if err != nil {
+			http.Error(w, "error preparing response data", http.StatusInternalServerError)
+			return
+		}
+
+		responseData, err := json.Marshal(resMetrics)
 		if err != nil {
 			http.Error(w, "error marshaling response data", http.StatusInternalServerError)
 			return
 		}
+		
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		w.Write(responseData)
 		
 	}
 	return http.HandlerFunc(fun)
+}
+
+func makeMetricsResult(metricType string, metricData config.StringAnswer) (models.Metrics, error) {
+	var resMetrics = models.Metrics{}
+	switch metricType{
+			case models.Counter:
+				delta, err := strconv.ParseInt(metricData.Value, 10, 64)
+				if err != nil {
+					return models.Metrics{}, fmt.Errorf("error parsing counter value")
+				}
+				resMetrics = models.Metrics{
+					ID: metricData.Name,
+					MType: metricData.Type,
+					Delta: &delta,
+				}
+			case models.Gauge:
+				value, err := strconv.ParseFloat(metricData.Value, 64)
+				if err != nil {
+					return models.Metrics{}, fmt.Errorf("error parsing gauge value")
+					
+				}
+				resMetrics = models.Metrics{
+					ID: metricData.Name,
+					MType: metricData.Type,
+					Value: &value,
+				}
+		}
+	return resMetrics, nil
 }
 
 func (mh *MetricsHandler) SetMetricDataHandle() http.Handler {
